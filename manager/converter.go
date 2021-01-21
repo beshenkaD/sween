e9aa22e88c18fca7622c11a4909da63d107b275e
beshenkaD <mambatamba6@gmail.com>
2021-01-21 21:45:31 +0900

    refactor(convert.go) now converter looks pretty and can undo the changes

--- manager/converter.go
+++ manager/converter.go
@@ -2,86 +2,124 @@ package manager
 
 import (
 	"fmt"
+	"github.com/beshenkaD/sween/utils"
 	. "github.com/otiai10/copy"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
-    "github.com/beshenkaD/sween/utils"
 )
 
-// copy target to wd
-// add target to manager.toml
-// remove target
-// link dotfile
+func createDotfile(path string, user string) Dotfile {
+	var (
+		source string
+		target string
+		hd     string // home dir
+	)
 
-func Convert(path string, user string) {
-	// 1st
-	wd, err := os.Getwd()
-	usr, err := os.UserHomeDir()
-
-	if err != nil {
-		panic(err)
+	if h, err := os.UserHomeDir(); err != nil {
+		utils.PrintError(err)
+		os.Exit(1)
+	} else {
+		hd = h
 	}
 
-	name := filepath.Base(path) // == source
+	source = filepath.Base(path)
+	target = path
 
-	err = Copy(path, wd+"/"+name)
-
-	if err != nil {
-		panic(err)
-	}
-
-	// 2nd
-	s := `
-[dotfiles.%s]
-    source = "%s"
-    target = "%s"
-`
 	if ext := filepath.Ext(path); ext != "" {
-		name = name[0 : len(name)-len(ext)]
+		source = source[0 : len(source)-len(ext)]
 	}
 	if strings.HasSuffix(path, "rc") {
-		name = strings.ReplaceAll(name, "rc", "")
+		source = strings.ReplaceAll(source, "rc", "")
+	}
+	if strings.HasPrefix(path, hd) {
+		target = strings.ReplaceAll(target, hd, "~")
 	}
 
-	ext := filepath.Ext(path)
-
-	target := path
-
-	if strings.HasPrefix(path, usr) {
-		target = strings.ReplaceAll(target, usr, "~")
-	}
-
-	str := fmt.Sprintf(s, name, name, target)
-
-	fmt.Println(ext)
-
-	f, err := os.OpenFile("manager.toml", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		panic(err)
-	}
-
-	defer f.Close()
-
-	if _, err = f.WriteString(str); err != nil {
-		panic(err)
-	}
-
-	//3rd Remove target
-	os.RemoveAll(path)
-
-	//4th link
 	newDotfile := Dotfile{
-		Source: name,
+		Source: source,
 		Target: target,
 		Hooks:  nil,
 	}
 
-	err = newDotfile.Operation(user, Link)
+	return newDotfile
+}
+
+func copyToDest(source string, target string) {
+	wd, err := os.Getwd()
 
 	if err != nil {
-        utils.PrintError(err)
+		utils.PrintError(err)
+		os.Exit(1)
 	}
 
-	os.Exit(0)
+	if err := Copy(source, wd+"/"+target); err != nil {
+		utils.PrintError(err)
+		os.Exit(1)
+	}
+}
+
+func writeToManager(template string, d Dotfile) {
+	f, err := os.OpenFile("manager.toml", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		utils.PrintError(err)
+		os.Exit(1)
+	}
+
+	if _, err = f.WriteString(template); err != nil {
+		utils.PrintError(err)
+		os.Exit(1)
+	}
+
+	f.Close()
+}
+
+func removeFromManager(strToRemove string) error {
+	f, err := ioutil.ReadFile("manager.toml")
+
+	if err != nil {
+		return err
+	}
+
+	newContents := strings.Replace(string(f), strToRemove, "", -1)
+
+	err = ioutil.WriteFile("manager.toml", []byte(newContents), 0)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Convert(path string, user string) {
+	dot := createDotfile(path, user)
+
+	template := `
+[dotfiles.%s]
+    source = "%s"
+    target = "%s"
+`
+	str := fmt.Sprintf(template, dot.Source, dot.Source, dot.Target)
+
+	copyToDest(path, dot.Source)
+	writeToManager(str, dot)
+
+	if err := os.RemoveAll(path); err != nil {
+		utils.PrintError(err)
+		os.Exit(1)
+	}
+
+	if err := dot.Operation(user, Link); err != nil {
+		if err := os.Rename(dot.Source, path); err != nil {
+			panic(err)
+		}
+
+        if err := removeFromManager(str); err != nil {
+            panic(err)
+        }
+		utils.PrintError(err)
+		os.Exit(1)
+	}
 }
